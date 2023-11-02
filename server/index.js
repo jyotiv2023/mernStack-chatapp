477b487f2effae893d076a90887686b20b4e0a81
Jyoti Verma <jyov2023@gmail.com>
2023-11-02 22:59:59 +0530

    changesss

--- server/index.js
+++ server/index.js
@@ -24,30 +24,24 @@ app.use("/api/chat", chatRoutes);
 app.use("/api/message", messageRoutes);
 
 //-------------deployment----------
-// // const __dirname1 = path.resolve();
-// const currentWorkingDirectory = path.resolve();
-// console.log(currentWorkingDirectory);
 
-// if (process.env.NODE_ENV === "production") {
+const currentWorkingDirectory = path.resolve();
 
-//   app.use(
-//     express.static(path.join(currentWorkingDirectory, "../client/build"))
-//   );
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    express.static(path.join(currentWorkingDirectory, "../client/build"))
+  );
 
-//   // const indexPath = path.resolve(
-//   //   currentWorkingDirectory,
-//   //   "../client/build/index.html"
-//   // );
-//   app.get("*", (req, res) =>
-//     res.sendFile(
-//       path.resolve(currentWorkingDirectory, "../client/build/index.html")
-//     )
-//   );
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("API is running successfully");
-//   });
-// }
+  const indexPath = path.resolve(
+    currentWorkingDirectory,
+    "../client/build/index.html"
+  );
+  app.get("*", (req, res) => res.sendFile(indexPath));
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running successfully");
+  });
+}
 //-------------deployment----------
 app.use(notFound);
 app.use(errorHandler);
